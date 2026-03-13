f6559ee3ac2a9b530a9e8d25940b38c75840ba71
blis608 <blissnjoki552@gmail.com>
2026-03-13 22:04:38 +0300

    Initialize main.js with user information logging
    Add variable declarations and console logs for user info

---
+++ main.js
@@ -0,0 +1,20 @@
+// Variable declarations
+
+let name = "Irene Njoki";
+let age = 19;
+let isStudent = true;
+let favoriteColors = ["black", "grey", "blue"];
+let todayDate = new Date(2026, 2, 14);
+
+// Console logs
+
+console.log("My name is:", name);
+console.log("My age is:", age);
+console.log("Am I a student?", isStudent);
+console.log("My favorite colors are:", favoriteColors);
+console.log("Today's date is:", todayDate);
+
+// typeof practice
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
